8e470752d2f3a9c8887dedcd2b4cfe8c3980d52c
James Kao <james@thekao.com>
2015-02-19 20:47:49 -0800

    hello world react

--- routes.js
+++ routes.js
@@ -1,13 +1,19 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
+/* Frontpage Routes */
 router.route('/')
   .get(function (req, res) {
     res.render('index')
   })
   .post(function (req, res) {
     res.send('Got a POST request')
+  })
+
+/* Hello World */
+router.route('/hello')
+  .get(function (req, res) {
+    res.render('hello')
   })
 
 module.exports = router
