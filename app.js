315525729daf0a9c0ef753cf6d6e149c252001f2
James Kao <james@thekao.com>
2015-02-19 19:04:15 -0800

    loggers and parsers

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express')
 var favicon = require('serve-favicon')
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express()
 
 app.set('port', (process.env.PORT || 3000))
 app.set('views', './views')
 app.set('view engine', 'jade')
-app.use(express.static(__dirname + '/public'))
+
 app.use(favicon(__dirname + '/public/favicon.ico'))
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'))
 
 app.route('/')
   .get(function (req, res) {
