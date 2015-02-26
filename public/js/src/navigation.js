b776f6039b738adbc202c31186a2ea7b620b51b9
James Kao <james@thekao.com>
2015-02-25 17:56:34 -0800

    navbar styles

---
+++ public/js/src/navigation.js
@@ -0,0 +1,64 @@
+var NAV_LINKS = {
+  'about': {
+    link: '/#about',
+    title: 'About Me'
+  },
+  'what-i-do': {
+    link: '/#what-i-do',
+    title: 'What I Do'
+  },
+  'background': {
+    link: '/#background',
+    title: 'Background'
+  },
+  'gallery': {
+    link: '/#gallery',
+    title: 'Gallery'
+  },
+  'contact': {
+    link: '/#contact',
+    title: "Let's Connect"
+  },
+  'testimonials': {
+    link: '/#testimonials',
+    title: "What Others Say"
+  }
+}
+
+var NavMain = React.createClass({
+  render: function() {
+    return (
+      <div className='NavMain' class="navbar navbar-inverse navbar-fixed-top">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+        </nav>
+      </div>
+    )
+  }
+})
+// <div className='NavMain'>
+//   <nav class="navbar navbar-inverse navbar-fixed-top">
+//     <div class="container">
+//       <div class="navbar-header">
+//         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+//           <span class="sr-only">Toggle navigation</span>
+//           <span class="icon-bar"></span>
+//           <span class="icon-bar"></span>
+//           <span class="icon-bar"></span>
+//         </button>
+//         <a class="navbar-brand" href="#">James Kao</a>
+//       </div>
+//       <div id="navbar" class="collapse navbar-collapse">
+//         <ul class="nav navbar-nav">
+//           <li class="active"><a href="#">Home</a></li>
+//           <li><a href="#about">About</a></li>
+//           <li><a href="#contact">Contact</a></li>
+//         </ul>
+//       </div><!--/.nav-collapse -->
+//     </div>
+//   </nav>
+// </div>
+
+React.render(
+  <NavMain />,
+  document.getElementById('navigation')
+)
